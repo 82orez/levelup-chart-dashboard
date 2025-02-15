d99d04bdee8934a01992fc9f60f398f4232b88f5
82orez <82orez@naver.com>
2025-02-15 15:31:11 +0900

    Log in first social second email - supense

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -52,7 +52,7 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.KAKAO_CLIENT_SECRET || "",
     }),
   ],
-  pages: { signIn: "/sign-in", error: "/error" },
+  pages: { signIn: "/sign-in" },
   callbacks: {
     jwt: async ({ user, token }) => {
       console.log("user: ", user);
@@ -94,7 +94,7 @@ export const authOptions: NextAuthOptions = {
 
         if (existingUser) {
           // * 반환값: 오류를 표시할 클라이언트 경로(여기에서는 /error + 쿼리문(여기에서는 error=EmailExists)
-          return "/error?error=EmailExists";
+          return "/users/sign-in?error=EmailExists";
         }
       }
 
