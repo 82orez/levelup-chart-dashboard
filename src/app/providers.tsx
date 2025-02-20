62db99cf0327b2228aefc70ec598f4a459ca448d
82orez <82orez@naver.com>
2025-02-21 07:59:08 +0900

    provider loading location changed-1

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -19,21 +19,25 @@ export const NextLayout = ({ children }: Props) => {
   console.log("status: ", status);
   console.log("data: ", data);
   const router = useRouter();
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-  // 로그인이 되어 있을 때 이 페이지로 접근하면 루트 페이지 '/dashboard' 로 되돌림.
+  // 로그인이 되어 있을 때 이 페이지로 접근하면 루트 페이지 '/dashboard' 로 되돌림.]\
+  // isLoading 의 초기 상태값을 true 로 지정.
   useEffect(() => {
-    setIsLoading(true);
-    if (status === "authenticated") {
+    if (status === "loading") {
+      setIsLoading(true);
+    } else if (status === "authenticated") {
       router.replace("/dashboard");
+      setIsLoading(false);
+    } else {
+      setIsLoading(false); // 인증되지 않은 상태면 로딩 해제
     }
-    setIsLoading(false);
   }, [status, router]);
 
   return (
     <div>
       {isLoading ? (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm">
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-100 bg-opacity-50 backdrop-blur-sm">
           <div className="flex flex-col items-center justify-center rounded-xl bg-white p-6 shadow-lg">
             {/* React Icons 로딩 아이콘 + Tailwind 애니메이션 적용 */}
             <FiLoader className="h-12 w-12 animate-spin text-blue-500" />
